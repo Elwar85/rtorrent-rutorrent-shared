7560baa42a61bd671e5ef02dcd6e57cefaf465af
Alex Kalvan <elwar@elwar.su>
2019-10-10 01:08:45 +0300

    Update startup-rtorrent.sh

--- startup-rtorrent.sh
+++ startup-rtorrent.sh
@@ -9,10 +9,7 @@ RT_GID_current=$(cat /etc/group | grep ^rtorrent | cut -d ":" -f3)
 
 # update uids and gids
 [[ "$RT_GID" != "$RT_GID_current" ]] && groupmod -g ${RT_GID} rtorrent
-useradd -u $RT_UID -g $RT_GID -d /home/rtorrent -m -s /bin/bash rtorrent
-if [ $? != 0 ]; then
-    adduser -u $RT_UID -G rtorrent -h /home/rtorrent -D -s /bin/ash rtorrent
-fi
+adduser -u $RT_UID -G rtorrent -h /home/rtorrent -D -s /bin/ash rtorrent
 
 # arrange dirs and configs
 mkdir -p /downloads/.rtorrent/session
