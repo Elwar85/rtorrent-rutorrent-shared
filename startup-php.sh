e7f08ffa4db276bff4bab0646ad10d3c4ff7772d
Al K <elwar@elwar.su>
2023-12-02 12:13:25 +0100

    Upgrade php version from 7 to 8

--- startup-php.sh
+++ startup-php.sh
@@ -4,11 +4,11 @@ set -x
 
 MEM=${PHP_MEM:=256M}
 
-sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php7/php.ini
-sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php7/php-fpm.conf
+sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php81/php.ini
+sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php81/php-fpm.conf
 
 mkdir -p /run/php
 mkdir -p /var/run/php
-php-fpm7 --nodaemonize
+php-fpm81 --nodaemonize
 php-fpm --nodaemonize
 
