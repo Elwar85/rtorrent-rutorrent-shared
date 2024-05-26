abb41910b7c872347ed4ff07d63ff9dda3c65270
Al K <elwar@elwar.su>
2024-05-26 17:14:19 +0000

    Upgrade PHH 8.1 to 8.3

--- startup-php.sh
+++ startup-php.sh
@@ -4,11 +4,11 @@ set -x
 
 MEM=${PHP_MEM:=256M}
 
-sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php81/php.ini
-sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php81/php-fpm.conf
+sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php83/php.ini
+sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php83/php-fpm.conf
 
 mkdir -p /run/php
 mkdir -p /var/run/php
-php-fpm81 --nodaemonize
+php-fpm83 --nodaemonize
 php-fpm --nodaemonize
 
