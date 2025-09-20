f6e2ad23522f06d44ce7ead3a45d25491356f9a2
Al K. <elwar@elwar.su>
2025-09-20 15:08:49 +0200

    Update startup-rtorrent.sh

--- startup-rtorrent.sh
+++ startup-rtorrent.sh
@@ -5,11 +5,12 @@ set -x
 # set rtorrent user and group id
 RT_UID=${USR_ID:=1000}
 RT_GID=${GRP_ID:=1000}
-RT_GID_current=$(cat /etc/group | grep ^rtorrent | cut -d ":" -f3)
+RT_GID_current=$(getent group rtorrent | cut -d: -f3)
 
 # update uids and gids
 [[ "$RT_GID" != "$RT_GID_current" ]] && groupmod -g ${RT_GID} rtorrent
-adduser -u $RT_UID -G rtorrent -h /home/rtorrent -D -s /bin/ash rtorrent
+id -u rtorrent >/dev/null 2>&1 || \
+  adduser -u "$RT_UID" -G rtorrent -h /home/rtorrent -D -s /bin/ash rtorrent
 
 # arrange dirs and configs
 mkdir -p /downloads/.rtorrent/session
@@ -18,7 +19,7 @@ mkdir -p /downloads/.log/rtorrent
 if [ ! -e /downloads/.rtorrent/.rtorrent.rc ]; then
     cp /root/.rtorrent.rc /downloads/.rtorrent/
 fi
-ln -s /downloads/.rtorrent/.rtorrent.rc /home/rtorrent/
+ln -sf /downloads/.rtorrent/.rtorrent.rc /home/rtorrent/
 chown -R rtorrent:rtorrent /downloads/.rtorrent
 chown -R rtorrent:rtorrent /home/rtorrent
 chown rtorrent:rtorrent /downloads/.log/rtorrent
@@ -27,4 +28,3 @@ rm -f /downloads/.rtorrent/session/rtorrent.lock
 
 # run
 su -l -c "TERM=xterm rtorrent" rtorrent
-
